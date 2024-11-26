b96dd90179f490676de3048d1e7d56fb1ace2580
Nereo <nereo.lauber@protonmail.com>
2024-11-26 10:32:46 +0100

    jacobi-2d mpi WIP - DOES NOT COMPILE

--- stencils/jacobi-2d/jacobi-2d_mpi.c
+++ stencils/jacobi-2d/jacobi-2d_mpi.c
@@ -10,29 +10,25 @@
 /* Include benchmark-specific header. */
 #include "jacobi-2d.h"
 
-
 /* Array initialization. */
-static
-void init_array (int n,
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-		 DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+static void init_array(int n,
+                       DATA_TYPE POLYBENCH_2D(A, N, N, n, n),
+                       DATA_TYPE POLYBENCH_2D(B, N, N, n, n))
 {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++)
-      {
-	A[i][j] = ((DATA_TYPE) i*(j+2) + 2) / n;
-	B[i][j] = ((DATA_TYPE) i*(j+3) + 3) / n;
-      }
+    {
+      A[i][j] = ((DATA_TYPE)i * (j + 2) + 2) / n;
+      B[i][j] = ((DATA_TYPE)i * (j + 3) + 3) / n;
+    }
 }
-
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
-static
-void print_array(int n,
-		 DATA_TYPE POLYBENCH_2D(A,N,N,n,n))
+static void print_array(int n,
+                        DATA_TYPE POLYBENCH_2D(A, N, N, n, n))
 
 {
   int i, j;
@@ -40,127 +36,107 @@ void print_array(int n,
   POLYBENCH_DUMP_START;
   POLYBENCH_DUMP_BEGIN("A");
   for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++) {
-      if ((i * n + j) % 20 == 0) fprintf(POLYBENCH_DUMP_TARGET, "\n");
+    for (j = 0; j < n; j++)
+    {
+      if ((i * n + j) % 20 == 0)
+        fprintf(POLYBENCH_DUMP_TARGET, "\n");
       fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, A[i][j]);
     }
   POLYBENCH_DUMP_END("A");
   POLYBENCH_DUMP_FINISH;
 }
 
-
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
-static
-void kernel_jacobi_2d(int tsteps,
-			    int n,
-			    DATA_TYPE POLYBENCH_2D(A,N,N,n,n),
-			    DATA_TYPE POLYBENCH_2D(B,N,N,n,n))
+static void kernel_jacobi_2d(int tsteps,
+                             int n,
+                             DATA_TYPE POLYBENCH_2D(A, N, N, n, n),
+                             DATA_TYPE POLYBENCH_2D(B, N, N, n, n),
+                             int rank,
+                             int size,
+                             int col_procs,
+                             int row_procs)
 {
   int t, i, j;
 
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  int n_rows = N / size;
-  int start = rank * n_rows + 1;
-  int end = start + n_rows - 1;
-
 #pragma scop
-  MPI_Request send_requests[2], recv_requests[2];
+  int p_col = rank % col_procs; // This block's col within all blocks
+  int p_row = rank / col_procs; // This block's row within all blocks
+  int block_length = n / col_procs;
+  int block_height = n / row_procs;
+  int start_col = p_col * block_length;
+  int start_row = p_row * block_height;
 
-  for (t = 0; t < TSTEPS; t++) {
-      // Exchange boundary rows with neighbors for B matrix update
-      if (rank > 0) { // Send top row and receive from above
-          MPI_Isend(&A[start][0], N, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &send_requests[0]);
-          MPI_Irecv(&A[start - 1][0], N, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &recv_requests[0]);
-      }
-      if (rank < size - 1) { // Send bottom row and receive from below
-          MPI_Isend(&A[end][0], N, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &send_requests[1]);
-          MPI_Irecv(&A[end + 1][0], N, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &recv_requests[1]);
-      }
+  for (t = 0; t < _PB_TSTEPS; t++)
+  {
+    // Block rows to be sent to other blocks
+    DATA_TYPE *s_top_row = &A[start_col][start_row];                       // to be send to upper block as lower buffer
+    DATA_TYPE *s_bottom_row = &A[start_col + block_height - 1][start_row]; // to be send to lower block as top buffer
 
-      // Wait for all communications to complete
-      if (rank > 0) MPI_Wait(&recv_requests[0], MPI_STATUS_IGNORE);
-      if (rank < size - 1) MPI_Wait(&recv_requests[1], MPI_STATUS_IGNORE);
+    // Block cols to be sent to other blocks
+    DATA_TYPE s_left_row[block_length];
+    DATA_TYPE s_right_row[block_length];
+    for (int i = start_row; i < start_row + block_height - 1; i++)
+    {
+      // Iterate over rows i={start_row...(start_row+block_height-1)} for A[start_row + i][...]
+      s_left_row[i] = A[i][start_col];
+      s_right_row[i] = A[i][start_col + block_length - 1];
+    }
 
-      // Compute B matrix
-      for (i = start; i <= end; i++) {
-          for (j = 1; j < N - 1; j++) {
-              B[i][j] = SCALAR_VAL(0.2) * (A[i][j] + A[i][j - 1] + A[i][j + 1] + A[i + 1][j] + A[i - 1][j]);
-          }
-      }
 
-      // Exchange boundary rows with neighbors for A matrix update
-      if (rank > 0) {
-          MPI_Isend(&B[start][0], N, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &send_requests[0]);
-          MPI_Irecv(&B[start - 1][0], N, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &recv_requests[0]);
-      }
-      if (rank < size - 1) {
-          MPI_Isend(&B[end][0], N, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &send_requests[1]);
-          MPI_Irecv(&B[end + 1][0], N, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &recv_requests[1]);
-      }
+    for (i = 1; i < _PB_N - 1; i++)
+      for (j = 1; j < _PB_N - 1; j++)
+        B[i][j] = SCALAR_VAL(0.2) * (A[i][j] + A[i][j - 1] + A[i][1 + j] + A[1 + i][j] + A[i - 1][j]);
 
-      // Wait for all communications to complete
-      if (rank > 0) MPI_Wait(&recv_requests[0], MPI_STATUS_IGNORE);
-      if (rank < size - 1) MPI_Wait(&recv_requests[1], MPI_STATUS_IGNORE);
-
-      // Compute A matrix
-      for (i = start; i <= end; i++) {
-          for (j = 1; j < N - 1; j++) {
-              A[i][j] = SCALAR_VAL(0.2) * (B[i][j] + B[i][j - 1] + B[i][j + 1] + B[i + 1][j] + B[i - 1][j]);
-          }
-      }
+    for (i = 1; i < _PB_N - 1; i++)
+      for (j = 1; j < _PB_N - 1; j++)
+        A[i][j] = SCALAR_VAL(0.2) * (B[i][j] + B[i][j - 1] + B[i][1 + j] + B[1 + i][j] + B[i - 1][j]);
   }
 #pragma endscop
-
 }
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  /* Init MPI */
-  MPI_Init(&argc, &argv);
-
   /* Retrieve problem size. */
   int n = N;
   int tsteps = TSTEPS;
-
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   /* Variable declaration/allocation. */
   POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
   POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, N, N, n, n);
 
+  /* MPI vars */
+  int rank, size;
+
+  /* Initialize MPI */
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  /* TODO: calculate number of row and col processes */
+
+  /* TODO: initialize arrays (remember to add 2 extra  rows and cols to accomodate overlap) */
+
   /* Initialize array(s). */
-  init_array (n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
+  init_array(n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
 
   /* Start timer. */
-  if (rank == 0)
-    polybench_start_instruments;
+  polybench_start_instruments;
 
   /* Run kernel. */
-  kernel_jacobi_2d(tsteps, n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
+  kernel_jacobi_2d(tsteps, n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), rank, size);
 
   /* Stop and print timer. */
-  if (rank == 0) {
-    polybench_stop_instruments;
-    polybench_print_instruments;
+  polybench_stop_instruments;
+  polybench_print_instruments;
 
-    /* Prevent dead-code elimination. All live-out data must be printed
-      by the function call in argument. */
-    polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(A)));
-  }
-  
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+  polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(A)));
+
   /* Be clean. */
   POLYBENCH_FREE_ARRAY(A);
   POLYBENCH_FREE_ARRAY(B);
-
-  /* Finalize MPI */
-  MPI_Finalize();
 
   return 0;
 }
